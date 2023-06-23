8f83e36699ed002dae15ba3b26fce49f34088d31
bloodsteelrain <chwoo9528@gmail.com>
2023-06-23 16:32:45 +0900

    (6/23) blog 포스트 댓글 태그 CRUD 구현

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 # blog/forms.py
 from django import forms
-from .models import Post, Comment
+from .models import Post, Comment, HashTag
 
 # Form: html에 있는 form 태그
 # Model Form: model을 사용하는 form
@@ -16,4 +16,13 @@ class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ['content']
+        widgets = {
+            'content': forms.Textarea(attrs={'rows': '3', 'cols': '35'})
+        }
 
+
+class HashTagForm(forms.ModelForm):
+
+    class Meta:
+        model = HashTag
+        fields = ['name']
